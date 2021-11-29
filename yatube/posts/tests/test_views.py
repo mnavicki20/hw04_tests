78399d7d084a4020d1af938f26a8eaea39aa61f5
Mikita Navicki <bulbanik91@gmail.com>
2021-11-29 17:54:08 +0300

    Тестирование Views: используются корректные шаблоны

---
+++ yatube/posts/tests/test_views.py
@@ -0,0 +1,49 @@
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+from django.urls import reverse
+
+from ..models import Post, Group
+
+User = get_user_model()
+
+
+class PostViewsTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.test_user = User.objects.create(
+            username='test_username',
+            email='testmail@gmail.com',
+            password='Qwerty123',
+        )
+        cls.group = Group.objects.create(
+            title='Тестовый заголовок',
+            slug='test-group',
+            description='Тестовое описание',
+        )
+        cls.post = Post.objects.create(
+            author=cls.test_user,
+            group=cls.group,
+            text='Тестовый текст',
+        )
+
+    def setUp(self):
+        self.guest_client = Client()
+        self.authorized_client = Client()
+        self.authorized_client.force_login(self.test_user)
+
+    # Проверка используемых шаблонов
+    def test_pages_use_correct_templates(self):
+        """URL-адрес использует корректный шаблон."""
+        templates_pages_names = {
+            'posts/index.html': reverse('posts:index'),
+            'posts/group_list.html': reverse('posts:group_posts', args=[PostViewsTest.group.slug]),
+            'posts/profile.html': reverse('posts:profile', args=[PostViewsTest.test_user.username]),
+            'posts/post_detail.html': reverse('posts:post_detail', args=[PostViewsTest.post.id]),
+            'posts/create_post.html': reverse('posts:post_edit', args=[PostViewsTest.post.id]),
+            'posts/create_post.html': reverse('posts:post_create'),
+        }
+        for template, reverse_name in templates_pages_names.items():
+            with self.subTest(reverse_name=reverse_name):
+                response = self.authorized_client.get(reverse_name)
+                self.assertTemplateUsed(response, template)
