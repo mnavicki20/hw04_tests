982af0f05e95ff06fcd10761cebdb63eadaa8810
Mikita Navicki <bulbanik91@gmail.com>
2021-12-02 12:02:29 +0300

    Добавлен тест на корректность передаваемого шаблона с картинкой

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -1,6 +1,11 @@
+import shutil
+import tempfile
+
 from django import forms
+from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.test import Client, TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import Client, TestCase, override_settings
 from django.urls import reverse
 
 from yatube.settings import ITEMS_PER_PAGE
@@ -9,7 +14,10 @@ from ..models import Group, Post
 
 User = get_user_model()
 
+TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
 
+
+@override_settings(MEDIA_ROOT=TEMP_MEDIA_ROOT)
 class PostViewsTest(TestCase):
     @classmethod
     def setUpClass(cls):
@@ -24,11 +32,30 @@ class PostViewsTest(TestCase):
             slug='test-group',
             description='Тестовое описание',
         )
+        small_gif = (
+            b'\x47\x49\x46\x38\x39\x61\x02\x00'
+            b'\x01\x00\x80\x00\x00\x00\x00\x00'
+            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+            b'\x0A\x00\x3B'
+        )
+        uploaded = SimpleUploadedFile(
+            name='small.gif',
+            content=small_gif,
+            content_type='image/gif'
+        )
         cls.post = Post.objects.create(
             author=cls.test_user,
             group=cls.group,
             text='Тестовый текст',
+            image=uploaded,
         )
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        shutil.rmtree(TEMP_MEDIA_ROOT, ignore_errors=True)
 
     def setUp(self):
         self.guest_client = Client()
@@ -96,6 +123,7 @@ class PostViewsTest(TestCase):
         self.assertEqual(first_object.text, self.post.text)
         self.assertEqual(first_object.author, self.post.author)
         self.assertEqual(first_object.group, self.post.group)
+        self.assertTrue(first_object.image, self.post.image)
 
     def test_profile_page_uses_correct_context(self):
         """Шаблон страницы пользователя сформирован с правильным контекстом."""
