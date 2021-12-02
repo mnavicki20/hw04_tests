2f9c4d243fa5b828ee4d47d7e63fb27306ad913d
Mikita Navicki <bulbanik91@gmail.com>
2021-12-02 09:23:07 +0300

    Написание теста для несуществующей страницы v.2

--- yatube/core/tests.py
+++ yatube/core/tests.py
@@ -8,7 +8,7 @@ class ViewTestClass(TestCase):
         """Проверка кодов ответа и используемого шаблона несуществующей
         страницы."""
         response = self.client.get('/nonexist-page/')
-        excepted_status_code = HTTPStatus.NOT_FOUND
-        excepted_template = 'core/404.html'
-        self.assertEqual(response.status_code, excepted_status_code)
-        self.assertTemplateUsed(response, excepted_template)
+        expected_status_code = HTTPStatus.NOT_FOUND
+        expected_template = 'core/404.html'
+        self.assertEqual(response.status_code, expected_status_code)
+        self.assertTemplateUsed(response, expected_template)
